3df4f4caa1fca51b38a9875bb9b0dfc8d3d75a3c
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-09-10 08:45:11 -0700

    update

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,8 +16,8 @@ listint_t *insert_node(listint_t **head, int number)
     new_node = malloc(sizeof(listint_t));
     if (new_node == NULL)
         return (NULL);
-
-new_node->n = number;
+    
+    new_node->n = number;
     new_node->next = NULL;
 
     // Handle the case of inserting at the head
