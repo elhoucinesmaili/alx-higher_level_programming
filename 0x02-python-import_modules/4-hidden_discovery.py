40e8901fc890b906a8a24740c3a03e05e4adea0d
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-09-12 06:57:12 -0700

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,8 +6,6 @@ if __name__ == "__main__":
 
     # Get all the names defined by the module
     names = dir(hidden_4)
-    
-    # Filter names that do not start with '__' and print them in alphabetical order
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
