d4168d3d5fda194522a08582090116cf2dfa5f8d
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-10-15 09:37:45 -0700

    gdgd

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,27 @@
-#!/usr/bin/python3
-"""This module defines a class Student"""
+ #!/usr/bin/python3
+"""Student module.
+
+Contains a Student class and some methods.
+"""
 
 
-class Student:
-    """Represent a student."""
+class Student():
+    """Defines a Student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
+        """Sets the necessary attributes for the Student object.
+
+        Args:
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
         return self.__dict__
