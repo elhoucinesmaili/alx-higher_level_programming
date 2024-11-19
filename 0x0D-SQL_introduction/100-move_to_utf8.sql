4c572e3e422622eab250a93ec91ee80fa6b75b44
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-19 10:00:14 -0800

    hedgdg

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,6 @@
--- Change the character set and collation of the entire database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Use the hbtn_0c_0 database
-USE hbtn_0c_0;
-
--- Convert the first_table to utf8mb4 character set and utf8mb4_unicode_ci collation
+-- Change character SET and COLLATE
+-- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+USE hbtn_0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Modify the 'name' column in first_table to have utf8mb4 character set and utf8mb4_unicode_ci collation
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
