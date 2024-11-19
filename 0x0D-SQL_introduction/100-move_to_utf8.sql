80f089a6fc8830f8460510ab4a11457ac4afa892
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-19 09:52:50 -0800

    hedgdg

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,11 @@
--- Convert the database to utf8mb4 character set and collation
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Change the character set and collation of the entire database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table first_table to utf8mb4 character set and collation
+-- Use the hbtn_0c_0 database
+USE hbtn_0c_0;
+
+-- Convert the first_table to utf8mb4 character set and utf8mb4_unicode_ci collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the 'name' column in the first_table to utf8mb4 character set and collation
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Modify the 'name' column in first_table to have utf8mb4 character set and utf8mb4_unicode_ci collation
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
