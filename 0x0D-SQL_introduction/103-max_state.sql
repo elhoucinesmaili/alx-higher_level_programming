fa85e955f41d8da3bc80c3e6a0025dddd830a6fc
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-19 10:30:01 -0800

    jhdgfgf

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,9 +1,5 @@
--- converts hbtn_0c_0 database to utf8
--- table: first_table to utf8
--- field: name to utf8
-
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Displays the max temperature of each state, ordered by state name.
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
