008da0b313784ecdf6f024db1f17191c65eac2a0
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-19 08:52:50 -0800

    hdgdge

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,2 @@
--- Creates the database hbtn_0c_0 if it does not exist
-CREATE DATABASE hbtn_0c_0 WHERE NOT EXISTS (
-    SELECT SCHEMA_NAME 
-    FROM INFORMATION_SCHEMA.SCHEMATA 
-    WHERE SCHEMA_NAME = 'hbtn_0c_0'
-);
+-- Creates the database hbtn_0c_1
+CREATE DATABASE IF NOT EXISTS hbtn_0c_1;
