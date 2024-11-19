f21081fde5a58d7f1fdb8731af70f554e8d60137
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-19 08:57:04 -0800

    hdgdge

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- Creates the database hbtn-0c_0
-CREATE database IF NOT EXISTS hbtn_0c_0;
+-- Create the database hbtn_0c_0 if it does not already exist
+CREATE DATABASE hbtn_0c_0
+    IF NOT EXISTS;
