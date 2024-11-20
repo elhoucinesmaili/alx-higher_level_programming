5b86dd6d205041fc68ab07ab869eac55eaceeb78
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-20 09:43:31 -0800

    hdgdg

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Select cities with their corresponding states, ordered by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
