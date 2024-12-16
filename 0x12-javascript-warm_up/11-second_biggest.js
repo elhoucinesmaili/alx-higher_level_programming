671bc337400fd1137f79398319587204bdc24826
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-16 08:51:51 -0800

    hdgd

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that searches the second biggest integer in the list of arguments.
+
+const args = process.argv.slice(2).map(Number).filter(num => !isNaN(num));
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
+}
