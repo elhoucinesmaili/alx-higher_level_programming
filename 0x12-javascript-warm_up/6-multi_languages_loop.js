04507792ab2ec5409bafb1ba64b133b2841d5455
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-16 08:01:56 -0800

    hdgd

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Script that prints 3 lines using an array and a loop
+
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+messages.forEach((message) => console.log(message));
