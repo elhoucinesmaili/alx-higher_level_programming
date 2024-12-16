7633c9a5c8e2f7c6f5c9a47c25bc82e8d64a2944
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-16 09:04:33 -0800

    hdgd

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Update this script to replace the value 12 with 89.
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject); // Initial object with value 12
+myObject.value = 89;   // Update the value to 89
+console.log(myObject); // Object after updating value
