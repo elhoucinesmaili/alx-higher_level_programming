edb6cbe5833d43b1233f1318c1d47599885e1856
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-19 08:24:22 -0800

    git*

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 // A function that returns the reversed version of a list
 
-exports.esrever = function (list) {
-  return list.reverse();
+exports.reverse = function (list) {
+  const reversedList = [];
+  let i = 0;
+  for (i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
 };
-
