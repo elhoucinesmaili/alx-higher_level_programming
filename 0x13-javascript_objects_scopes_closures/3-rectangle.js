4d847cb10231b4835fd3146446383c8ba88bd4c1
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-19 08:54:44 -0800

    dhdg

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,20 +2,17 @@
 // A class Rectangle that defines a rectangle
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
-    // If conditions are not met, the object remains empty
   }
 
-  // Method to print the rectangle using 'X'
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = 'X'.repeat(this.width);
+      console.log(row);
     }
   }
 }
