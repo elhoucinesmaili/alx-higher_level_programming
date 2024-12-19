0044b729b9a5904652caa616793eb7475886e237
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-19 08:48:06 -0800

    git

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
-// A class Square that defines a square and inherits from Rectangle
-
+/*  class Square that defines a square and
+inherits from Rectangle of 4-rectangle.js */
 const Rectangle = require('./4-rectangle');
-
 class Square extends Rectangle {
-  constructor(size) {
-    super(size, size); // Call the parent class constructor with width and height equal to size
+  constructor (size) {
+    super(size, size);
+  }
+
+  // Override the print method from Rectangle class
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 }
-
 module.exports = Square;
