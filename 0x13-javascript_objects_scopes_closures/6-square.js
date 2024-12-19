1ad4a3c979da25c30a30e464e55d3f99ed6d4fff
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-19 08:46:34 -0800

    git

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-// A class Square that defines a square and inherits from Square
-
-const PrevSquare = require('./5-square');
-
-class Square extends PrevSquare {
-  charPrint(c) {
-    const char = c || 'X'; // Use 'X' if c is undefined
-    for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.width));
+// class Square that defines a square and inherits from Square of 5-square.js
+const Squa = require('./5-square.js');
+class Square extends Squa {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
     }
   }
 }
-
 module.exports = Square;
