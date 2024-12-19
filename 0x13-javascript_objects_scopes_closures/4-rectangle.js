b5403f0de8b80f25d39d5d1ed307bf173456a18f
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-19 08:50:01 -0800

    git

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,39 +1,30 @@
 #!/usr/bin/node
-// A class Rectangle that defines a rectangle
-
+// class Rectangle that defines a rectangle with additional methods
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  // Method to print the rectangle using 'X'
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('*'.repeat(this.width));
     }
   }
 
-  // Method to rotate the rectangle (swap width and height)
-  rotate() {
-    if (this.width && this.height) {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width]; // Using destructuring for a cleaner swap
   }
 
-  // Method to double the dimensions of the rectangle
-  double() {
-    if (this.width && this.height) {
-      this.width *= 2;
-      this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  area () {
+    return this.width * this.height;
   }
 }
-
 module.exports = Rectangle;
