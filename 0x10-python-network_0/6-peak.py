b69a58d5b14938ed5a13e38df59a8c8fc72fd8c6
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-16 08:01:14 -0800

    git

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+def find_peak(list_of_integers):
+    """Find a peak element in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        # Compare middle element with its neighbors to decide direction
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
