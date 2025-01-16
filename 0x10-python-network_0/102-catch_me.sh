a53188b47e490683410c13897ce9063d60a35c60
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-16 09:02:41 -0800

    git

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes reuest that causes the server to respond with message
-curl -s -L -X PUT -H "Origin: AlxSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
