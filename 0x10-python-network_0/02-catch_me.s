850a30c84fa303b477199ce31b3ee22025717abb
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-16 09:00:43 -0800

    git

--- 0x10-python-network_0/02-catch_me.s
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing "You got me!"
-curl -o /dev/null -s -w "You got me!" 0.0.0.0:5000/catch_me
