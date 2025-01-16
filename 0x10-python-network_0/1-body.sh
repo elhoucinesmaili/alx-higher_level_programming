022d9ae407f3714ab8d0b2931967f84869c700fe
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-16 07:35:51 -0800

    git

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request, and displays the body of a 200 status code response
-curl -s -L -X GET "$1" -o - -w "%{http_code}" | awk '{if ($NF == "200") {sub($NF,""); print}}'
+curl -sL "$1"
