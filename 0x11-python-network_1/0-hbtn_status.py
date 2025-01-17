cf4b29603e71ab2cd4d9f662365b5603e7a6395c
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-17 08:16:58 -0800

    git

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+and displays information about the response body.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
